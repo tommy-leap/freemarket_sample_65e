e02c29da01527b2de75bf398e59261860a88c9ac
nuigurumi03 <nuigurumi03@gmail.com>
2019-12-20 12:27:53 +0900

    Basic認証の実装

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,8 @@
 server '18.177.132.122', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
