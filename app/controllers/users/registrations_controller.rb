fdcdb394211c89a7bdc99934fa0166dcffddd8c8
shumpei-515 <shumpei-515@yahoo.co.jp>
2019-12-27 20:36:14 +0900

    結局step2まで

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,6 +15,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def step2
+    super
+  end
   # GET /resource/edit
   # def edit
   #   super
@@ -39,25 +42,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
   }
-
+  devise_scope :user do
+    get 'step1', to: 'users/registrations#new_step1'
+    post 'step1',  to: 'users/registrations#create_step1'
+  end  
   root 'users#index'
   get "signup", to: "signup#index"
   get 'products/error'
   resources :products, only: [:index, :new, :create, :show]
   resources :users, only: [:index, :show, :edit, :update]
 
-  resources :signup, only: [:new]do
+  resources :signup, only: [:new ]do
     collection do
       get 'step1'
       post 'step2'
