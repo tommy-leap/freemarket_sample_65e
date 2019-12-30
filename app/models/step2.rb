2489bb11ff18ba45647526eab7f6a1efda237fdd
nuigurumi03 <nuigurumi03@gmail.com>
2019-12-30 17:39:22 +0900

    step2修正

--- app/models/step2.rb
+++ app/models/step2.rb
@@ -2,5 +2,5 @@ class Step2 < ApplicationRecord
   belongs_to :user
   belongs_to :user_detail
 
-  #validates :null: false, format: { with:/\A0[5789]0[-(]?\d{4}[-)]?\d{4}\z/}
+  validates presnse: true, format: { with:/\A0[5789]0[-(]?\d{4}[-)]?\d{4}\z/}
 end
