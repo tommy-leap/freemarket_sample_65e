9fb3f2bba0879b619fa6221257514d876a10c2f7
tommy-leap <tomohira07@icloud.com>
2019-12-30 14:07:00 +0900

    商品詳細ページ　修正しました。

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,16 +1,15 @@
 $(function() {
   $('img.thumb').mouseover(function(){
-  var selectedSrc = $(this).attr('src').replace(/^(.+)_thumb(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
+    var selectedSrc = $(this).attr('src').replace(/^(.+)_thumb(\.gif|\.jpg|\.png+)$/, "$1"+"$2");
   
   $('img.mainImage').stop().fadeOut(50,
-  function(){
-  $('img.mainImage').attr('src', selectedSrc);
-  $('img.mainImage').stop().fadeIn(200);
-  }
-  );
+    function(){
+      $('img.mainImage').attr('src', selectedSrc);
+      $('img.mainImage').stop().fadeIn(200);
+      }
+    );
   });
-  
-  $('img.thumb').mouseout(function(){
-  $(this).css({"border":""});
+    $('img.thumb').mouseout(function(){
+    $(this).css({"border":""});
   });
 });
