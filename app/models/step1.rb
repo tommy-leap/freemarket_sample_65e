45841ba4c4233693f8dd7a5613a12f3d77359eda
nuigurumi03 <nuigurumi03@gmail.com>
2019-12-30 17:59:56 +0900

    軽微な修正(step2)

--- app/models/step1.rb
+++ app/models/step1.rb
@@ -3,7 +3,7 @@ class Step1 < ApplicationRecord
   belongs_to :user_detail
 
   validates :nickname, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([\A@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :encrypted_password, presence: true, uniqueness: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}\z/i}
   validates :last_name, null: false, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
   validates :first_name, null: false, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
