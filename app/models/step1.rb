0feeb4fb3481ada4fe202d796ad596d4a7ee978b
nuigurumi03 <nuigurumi03@gmail.com>
2019-12-30 17:56:20 +0900

    軽微な修正

--- app/models/step1.rb
+++ app/models/step1.rb
@@ -4,7 +4,7 @@ class Step1 < ApplicationRecord
 
   validates :nickname, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-  validates :encrypted_password, presence: true, uniqueness: true, format: { with: /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}\z/i}
+  validates :encrypted_password, presence: true, uniqueness: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}\z/i}
   validates :last_name, null: false, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
   validates :first_name, null: false, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
   validates :last_kana, null: false, format: { with: /\A[ァ-ヶー－]+\z/}
