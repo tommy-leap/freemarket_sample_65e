3becd54e2ab762ca2f918d17a14429aae00e3a2e
shumpei-515 <shumpei-515@yahoo.co.jp>
2020-01-11 13:19:51 +0900

    SNS認証 コントローラまで

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,32 +2,31 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+
     callback_for(:facebook)
     
   end
 
   def google_oauth2
-
+  
     callback_for(:google)
   end
 
-  # def passthru
-  # end
-
-
-
   def callback_for(provider)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted?
+    @omniauth = request.env['omniauth.auth']
+    info = User.find_oauth(@omniauth)
+    @user = info[:user]
+    if @user.persisted? 
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-    else
+    else 
       @sns = info[:sns]
-      render template: "signup/step1" 
+      render template: "devise/registrations/step1" 
+      # redirect_to new_user_registration_url
     end
   end
 
   def failure
-    redirect_to root_path, alert: 'Facebook認証に失敗しました。'
+    redirect_to root_path and return
   end
 end
