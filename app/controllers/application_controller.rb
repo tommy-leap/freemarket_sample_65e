c51be6bb5ce8e4077c7cd12b4eb4fb31844defa0
tommy-leap <tomohira07@icloud.com>
2020-01-13 12:16:01 +0900

    login修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource) 
+    root_path
+  end
 
   private
 
