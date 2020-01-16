9619d02cdd5ffe22d80076e7a2490dc35b98aac7
yuki-choki <ikuy0105@gmail.com>
2020-01-16 15:12:25 +0900

    ヘッダー、フッターを非表示
    各ページ、ロゴマークの遷移先を「root_path」に変更

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -51,25 +51,6 @@ class CardController < ApplicationController
     # redirect_to action: "show" if card.exists?
   end
 
-  # def step3
-  #   binding.pry
-  #   if params['payjp-token'].blank?
-  #     redirect_to action: "edit", id: current_user.id
-  #   else
-  #     customer = Payjp::Customer.create(
-  #     email: current_user.email,
-  #     card: params['payjp-token'],
-  #     metadata: {user_id: current_user.id}
-  #     )
-  #     @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-  #     if @card.save
-  #       redirect_to action: "step4"
-  #     else
-  #       redirect_to action: "step3", id: current_user.id
-  #     end
-  #   end
-  # end
-
   def step4
   end
 
